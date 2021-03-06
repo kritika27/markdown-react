4560d7b028bb2aefee44a078c0e58867bc596d9d
kritika <kritikasri27@gmail.com>
2021-03-07 00:38:39 +0530

    Added Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav id="nav">
+        <div className="container">
+          <h1 className="logo">Markdown Previewer</h1>
+        </div>
+      </nav>
+    </div>
+  );
+}
